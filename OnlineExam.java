de6b7b91d853316c84975bb5202fcd56b31986a6
abburinagasaiajaykumar116 <109759425+abburinagasaiajaykumar116@users.noreply.github.com>
2023-03-26 13:28:44 +0530

    Add files via upload

---
+++ OnlineExam.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.lang.Exception;
+import java.util.Timer;
+import java.util.TimerTask;
+class OnlineExam
+{
+    public static void main(String args[])
+    {
+        try
+        {
+            login form = new login();
+            form.setSize(400,150);
+            form.setVisible(true);
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+} 
